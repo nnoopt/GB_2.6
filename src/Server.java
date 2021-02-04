51c45ab9f75496d878f2cf86b24f9c58dc514e92
nnoopt <nnoopt@gmail.com>
2021-02-04 19:32:22 +0300

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,58 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+        Socket clientSocket = null;
+        Scanner scanner = new Scanner(System.in);
+
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            System.out.println("Server started");
+
+            clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getInetAddress());
+            DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+            Thread threadReader = new Thread(() -> {
+                try {
+                    while (true) {
+                        dataOutputStream.writeUTF(scanner.nextLine());
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            threadReader.setDaemon(true);
+            threadReader.start();
+
+            while (true) {
+                String str = inputStream.readUTF();
+                if (str.equals("/close")) {
+                    System.out.println("Клиент покинул сервер");
+                    dataOutputStream.writeUTF("/close");
+                    break;
+                } else {
+                    System.out.println("Клиент " + str);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                clientSocket.close();
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+}
+
